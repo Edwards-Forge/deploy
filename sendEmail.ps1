2d3cb8184681a7eaf0c59fef08147b15a9d11576
Edwards-Forge <102234171+Edwards-Forge@users.noreply.github.com>
2022-04-22 13:24:10 +0100

    Add files via upload

---
+++ sendEmail.ps1
@@ -0,0 +1,22 @@
+﻿$Username = "jack@edwardsforge.com";
+$Password = "jijxowleizueaprg";
+#$path = "C:\attachment.txt";
+
+function Send-ToEmail([string]$email, [string]$attachmentpath){
+
+    $message = new-object Net.Mail.MailMessage;
+    $message.From = "jack@edwardsforge.com";
+    $message.To.Add($email);
+    $message.Subject = "subject text here...";
+    $message.Body = "body text here...";
+   # $attachment = New-Object Net.Mail.Attachment($attachmentpath);
+   # $message.Attachments.Add($attachment);
+
+    $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", "587");
+    $smtp.EnableSSL = $true;
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    $smtp.send($message);
+    write-host "Mail Sent" ; 
+    #$attachment.Dispose();
+ }
+Send-ToEmail  -email "jackrabbitx2@googlemail.com" -attachmentpath $path;
