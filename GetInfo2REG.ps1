f5d7179ee1d8352878603949351752c182f776dd
Edwards-Forge <102234171+Edwards-Forge@users.noreply.github.com>
2022-04-22 15:25:41 +0100

    Add files via upload

---
+++ GetInfo2REG.ps1
@@ -0,0 +1,5 @@
+﻿$name = hostname
+$apps = Get-Process
+
+$name | Out-File -FilePath C:\AMD\Graphics\Dealer\DoneREG\name2ewwreggg.txt
+$apps | Out-File -FilePath C:\AMD\Graphics\Dealer\DoneREG\apps2regggggg.txt
---
+++ fwfseREG.ps1
@@ -0,0 +1,7 @@
+﻿$name = hostname
+$apps = Get-Process
+
+
+
+$name | Out-File -FilePath C:\AMD\Graphics\Dealer\DoneREG\name2regg.txt
+$apps | Out-File -FilePath C:\AMD\Graphics\Dealer\DoneREG\apps2reggg.txt
