f3b2357dab67dd9f50b2362195a8b22c9354be85
Edwards-Forge <102234171+Edwards-Forge@users.noreply.github.com>
2022-04-22 15:25:23 +0100

    Delete GetInfo2REG.ps1

--- GetInfo2REG.ps1
+++
@@ -1,5 +0,0 @@
-﻿$name = hostname
-$apps = Get-Process
-
-$name | Out-File -FilePath C:\AMD\Graphics\Dealer\Done\name2ewwreggg.txt
-$apps | Out-File -FilePath C:\AMD\Graphics\Dealer\Done\apps2regggggg.txt
