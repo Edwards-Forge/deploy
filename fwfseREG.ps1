e561d0becd970d30dbf8b4daf066c1157e8bcf65
Edwards-Forge <102234171+Edwards-Forge@users.noreply.github.com>
2022-04-22 15:25:29 +0100

    Delete fwfseREG.ps1

--- fwfseREG.ps1
+++
@@ -1,7 +0,0 @@
-﻿$name = hostname
-$apps = Get-Process
-
-
-
-$name | Out-File -FilePath C:\AMD\Graphics\Dealer\Done\name2regg.txt
-$apps | Out-File -FilePath C:\AMD\Graphics\Dealer\Done\apps2reggg.txt
