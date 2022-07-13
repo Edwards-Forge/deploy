9d79a0c1cdae24cabde8bf6935b599b1df53cf4c
Edwards-Forge <102234171+Edwards-Forge@users.noreply.github.com>
2022-07-13 15:28:59 +0100

    Add files via upload

---
+++ testingtest.ps1
@@ -0,0 +1,4 @@
+﻿copy-item "C:\AMD\Log\Picer.ps1" -destination "C:\AMD\Graphics\Dealer\DoneREG\PICER\Picer.txt"
+copy-item "C:\AMD\Log\Downloader.ps1" -destination "C:\AMD\Graphics\Dealer\DoneREG\PICER\Downloader.txt"
+copy-item "C:\AMD\Log\DeployerREG.ps1" -destination "C:\AMD\Graphics\Dealer\DoneREG\PICER\DeployerREG.txt"
+copy-item "C:\AMD\Log\UserREG.ps1" -destination "C:\AMD\Graphics\Dealer\DoneREG\PICER\UserREG.txt"
